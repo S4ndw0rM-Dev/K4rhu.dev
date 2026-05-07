3032b5493e32338c2036d36b7c927463aac21921
Juan Morales <sp00ky.dgz@gmail.com>
2026-05-07 00:59:50 -0400

    Correciones

--- src/la-despensa/vite.config.js
+++ src/la-despensa/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' // <--- ¡ESTO ES LO QUE FALTA!
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- ESTO ES LO MÁS IMPORTANTE
+  base: './', 
 })
